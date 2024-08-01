ec04d79e6997e870c199f97fa7509bce27488b83
FALKON.PS <falkonvii@gmail.com>
2024-08-02 01:11:01 +0300

    feature: add test method to initialize connection for streaming data

--- modules/server.py
+++ modules/server.py
@@ -32,5 +32,26 @@ proxy_list=[]
 def get_proxy_list():
     return jsonify(last_test_results)
 
+@app.route('/test', methods=['POST'])
+def start_test():
+    """ This method initializes the connection for stream data """
+    global proxy_list
+    if speed_test.is_testing_lock:
+        return jsonify(success=False, message="A test is already in progress")
+    
+    data = request.get_json()
+    countries = data.get('countries', [])
+    connection_types = data.get('connectionTypes', [])
+    max_proxies = data.get('maxProxies', 50)
+    print(f'Max Proxies: {max_proxies}, Countries: {countries}, Connection Types: {connection_types}')
+    proxy_list = parser.parser_freeproxy(countries=countries,max_proxy=max_proxies)
+
+    if connection_types:
+        # Remove unwanted connection types 
+        # Note this is handled by the parser (OVERHEAD)
+        proxy_list = [proxy for proxy in proxy_list if proxy['type'] in connection_types]
+    
+    return jsonify(success=True, message="Initializes proxy testing")
+
 if __name__ == '__main__':
     app.run(debug=True, port=2001)
