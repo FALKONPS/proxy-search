809b7ffb2263bc79ddfe14bd4c48fc043d365320
FALKON.PS <falkonvii@gmail.com>
2024-08-02 00:50:32 +0300

    feature: add API method to retrieve proxy list

--- modules/server.py
+++ modules/server.py
@@ -3,14 +3,34 @@ from flask_cors import CORS
 import requests
 import time
 import json
+import parser
+import speed_test
 
 app = Flask(__name__)
 
 CORS(app)
 
-last_test_results = []
+last_test_results = [  {
+    "address": "test1",
+    "country": "Germany",
+    "city": "Leipzig",
+    "type": "http",
+    "anonymity": "High",
+    "speed": "70"
+  },
+  {
+    "address": "test2",
+    "country": "Germany",
+    "city": "Muenster",
+    "type": "http",
+    "anonymity": "No",
+    "speed": "100"
+  }]
 last_test_file = 'last_test.json'
-
+proxy_list=[]
+@app.route('/proxy_list', methods=['GET'])
+def get_proxy_list():
+    return jsonify(last_test_results)
 
 if __name__ == '__main__':
     app.run(debug=True, port=2001)
